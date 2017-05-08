e5246b39bd74387b913a55c9af48347dce703fb6
Yacine Badiss <ybadiss@kpler.com>
2017-05-08 20:23:38 +0200

    check the github secret before applying changes

--- source/server.py
+++ source/server.py
@@ -1,9 +1,14 @@
-from flask import Flask, jsonify
+from hashlib import sha1
+import hmac
+import os
+
+from flask import Flask, jsonify, abort
 
 from store import Store
 
 app = Flask(__name__)
 store = Store()
+secret = os.environ['GITHUB_SECRET']
 
 
 @app.route('/')
@@ -19,6 +24,20 @@ def releases():
 
 @app.route('/githook', methods=['POST'])
 def githook():
+    header_signature = request.headers.get('X-Hub-Signature')
+    if not header_signature:
+        abort(403)
+
+    sha_name, signature = header_signature.split('=')
+    if sha_name != 'sha1':
+        abort(501)
+
+    # HMAC requires the key to be bytes, but data is string
+    mac = hmac.new(secret, msg=request.data, digestmod=sha1)
+
+    if not hmac.compare_digest(mac.hexdigest(), signature):
+        abort(403)
+
     open('/home/ubuntu/.restart', 'a').close()
     return 'ok'
 
