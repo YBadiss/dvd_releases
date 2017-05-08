ba786af84282aa25cd2f316a10fa438c16316015
Yacine Badiss <ybadiss@kpler.com>
2017-05-08 20:30:50 +0200

    improve release script

--- check_n_restart.sh
+++ check_n_restart.sh
@@ -7,13 +7,11 @@ then
     echo "Pulling and restarting"
     rm -f $FILE
 
-    cd /home/ubuntu
-    rm -rf dvd_releases
-    git clone https://github.com/YBadiss/dvd_releases.git
+    sudo rm -rf /home/ubuntu/dvd_releases
+    git clone https://github.com/YBadiss/dvd_releases.git /home/ubuntu/dvd_releases
 
-    cd dvd_releases
-    sudo cp configs/supervisord.conf /etc/supervisord.conf
-    crontab < configs/crontab
+    sudo cp /home/ubuntu/dvd_releases/configs/supervisord.conf /etc/supervisord.conf
+    crontab < /home/ubuntu/dvd_releases/configs/crontab
 
     /usr/local/bin/supervisorctl reread
     /usr/local/bin/supervisorctl restart all
