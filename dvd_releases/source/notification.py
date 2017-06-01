4097aa625da6ed4f25a0111d28345dbb902e2e44
Yacine Badiss <ybadiss@kpler.com>
2017-06-01 23:02:05 +0200

    fixed #7, a single notif is sent at a time and movie ids are passed in data

--- dvd_releases/source/notification.py
+++ dvd_releases/source/notification.py
@@ -11,23 +11,39 @@ logger.setLevel(logging.INFO)
 
 def check_new_notifications():
     movies = filter(lambda m: m.is_released_today, get_new_dvd_releases())
-    logger.info("Movies to notify found: {}".format(movies))
+    if movies:
+        logger.info("Sending notifications for movies: {}".format(movies))
+        _send_new_releases_notification(movies)
+    else:
+        logger.info("No notifications to send")
+
+
+def _send_new_releases_notification(movies):
+    _send_notification(message="Movies you like are now on DVD!",
+                       filters=_build_filters(movies),
+                       data=_build_data(movies))
+
+
+def _build_filters(movies):
+    filters = []
     for movie in movies:
-        _send_new_release_notification(movie)
+        filters.append({
+            "field": "tag",
+            "key": movie.id_,
+            "relation": "exists"
+        })
+        filters.append({"operator": "OR"})
+    filters.pop()  # remove last OR operator
+    return filters
 
 
-def _send_new_release_notification(movie):
-    filter = {
-        "field": "tag",
-        "key": movie.id_,
-        "relation": "=",
-        "value": "true"
+def _build_data(movies):
+    return {
+        "movie_ids": json.dumps([movie.id_ for movie in movies])
     }
-    message = "'{}' was released today in DVD!".format(movie.title)
-    _send_notification(message, filter)
 
 
-def _send_notification(message, filter):
+def _send_notification(message, filters, data):
     header = {
         "Content-Type": "application/json; charset=utf-8",
         "Authorization": "Basic {}".format(ONE_SIGNAL_AUTH_TOKEN)
@@ -35,12 +51,11 @@ def _send_notification(message, filter):
 
     payload = {
         "app_id": "1a8f6a98-26b6-4b18-b1a6-ce8f7996f89d",
-        "filters": [
-            filter
-        ],
+        "filters": filters,
         "contents": {
             "en": message
-        }
+        },
+        "data": data
     }
 
     requests.post("https://onesignal.com/api/v1/notifications",
